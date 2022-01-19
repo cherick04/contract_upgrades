c896769eb4e015a4a00e02a053638aaa8f792a40
Erick C <cherick04@hotmail.com>
2022-01-19 13:46:57 -0300

    Upgrading contract using proxy

---
+++ scripts/deploy_and_upgrade.py
@@ -0,0 +1,44 @@
+from scripts.helpful_scripts import get_account, encode_function_data, upgrade
+from brownie import (
+    accounts,
+    network,
+    Box,
+    BoxV2,
+    ProxyAdmin,
+    TransparentUpgradeableProxy,
+    Contract,
+)
+
+
+def main():
+    account = get_account()
+    print(f"Deploying to {network.show_active()}")
+    box = Box.deploy({"from": account})
+
+    proxy_admin = ProxyAdmin.deploy({"from": account})
+
+    # initializer = box.store, 1
+    box_encoded_initializer_function = encode_function_data()
+
+    proxy = TransparentUpgradeableProxy.deploy(
+        box.address,
+        proxy_admin.address,
+        box_encoded_initializer_function,
+        {"from": account, "gas_limit": 1000000},
+    )
+    print(f"Proxy deployed to {proxy}, you can now upgrade to V2!")
+
+    # Instead of calling a function directly from the contract, we call it
+    # on the proxy, and this will call the function for us
+    proxy_box = Contract.from_abi("Box", proxy.address, Box.abi)
+    proxy_box.store(1, {"from": account})
+
+    # Upgrade
+    box_v2 = BoxV2.deploy({"from": account})
+    upgrade_transaction = upgrade(
+        account, proxy, box_v2.address, proxy_admin_contract=proxy_admin
+    )
+    print("Proxy has been upgraded!")
+    proxy_box = Contract.from_abi("BoxV2", proxy.address, BoxV2.abi)
+    proxy_box.increment({"from": account})
+    print(proxy_box.retrieve())
---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,70 @@
+from hashlib import new
+from brownie import network, accounts, config
+import eth_utils
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local", "mainnet-fork"]
+
+
+def get_account(index=None, id=None):
+    # accounts[0]
+    # accounts.add("env")
+    # accounts.load("id")
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        return accounts[0]
+    if network.show_active() in config["networks"]:
+        return accounts.add(config["wallets"]["from_key"])
+    return None
+
+
+# Encode arguments into bytes, so that the contract knows what function to call
+def encode_function_data(initializer=None, *args):
+    """Encodes the function call so we can work with an initializer.
+    Args:
+        initializer ([brownie.network.contract.ContractTx], optional):
+        The initializer function we want to call. Example: `box.store`.
+        Defaults to None.
+        args (Any, optional):
+        The arguments to pass to the initializer function
+    Returns:
+        [bytes]: Return the encoded bytes.
+    """
+    if len(args) == 0 or not initializer:
+        return eth_utils.to_bytes(hexstr="0x")
+    else:
+        return initializer.encode_input(*args)
+
+
+def upgrade(
+    account,
+    proxy,
+    new_implementation_address,
+    proxy_admin_contract=None,
+    initializer=None,
+    *args
+):
+    if proxy_admin_contract:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            transaction = proxy_admin_contract.upgradeAndCall(
+                proxy.address,
+                new_implementation_address,
+                encoded_function_call,
+                {"from": account},
+            )
+        else:
+            transaction = proxy_admin_contract.upgrade(
+                proxy.address, new_implementation_address, {"from": account}
+            )
+    else:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            transaction = proxy.upgradeToAndCall(
+                new_implementation_address, encoded_function_call, {"from": account}
+            )
+        else:
+            transaction = proxy.upgradeTo(new_implementation_address, {"from": account})
+    return transaction
